163508b1ac59c3667d49eb28671711c2b326e4a8
shadhini <shadhini.jayatilake@gmail.com>
2023-09-14 11:26:46 +0530

    map filter reduce

---
+++ python_learn/src/Map_Filter_Reduce/exercise_01.py
@@ -0,0 +1,22 @@
+from functools import reduce
+
+# Use map to print the square of each numbers rounded
+# to three decimal places
+my_floats = [4.35, 6.09, 3.25, 9.77, 2.16, 8.88, 4.59]
+
+# Use filter to print only the names that are less than
+# or equal to seven letters
+my_names = ["olumide", "akinremi", "josiah", "temidayo", "omoseun"]
+
+# Use reduce to print the product of these numbers
+my_numbers = [4, 6, 9, 23, 5]
+
+if __name__ == "__main__":
+    # Fix all three respectively.
+    map_result = list(map(lambda x: round(x**2, 3), my_floats))
+    filter_result = list(filter(lambda name: True if len(name) <= 7 else False, my_names))
+    reduce_result = reduce(lambda num1, num2: num1 * num2, my_numbers)
+
+    print(map_result)
+    print(filter_result)
+    print(reduce_result)
